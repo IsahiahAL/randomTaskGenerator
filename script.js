6ed26ad91220b199015c1201ebdf5e29a71c5f89
Isahiah <103591562+IsahiahAL@users.noreply.github.com>
2024-10-21 23:56:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let task = document.getElementById('task');
+const newTask = document.getElementById('newTask');
+let taskList = ['30 min walk', '30 min bible study', 'full body stretch', 'cardio', 'workout', 'healthy snack', 'healthy meal', '30 min free code camp',
+                '3o min ztm', 'sweep', 'vacuum', 'call mom', 'call dad', 'call andrew', 'movie maker', '30 min piano', 'walk halo', 'clean cat litter',
+                'sweep and mop', 'learn a card trick', 'say five nice things to cheyanne', '30 min easyway'
+];
+
+
+
+
+
+function generateNewTask (){
+    let number = Math.floor(Math.random() * taskList.length);
+    task.innerHTML = taskList[number];
+    console.log(number);
+
+}
+
+
+
+
+newTask.addEventListener('click', generateNewTask);
+
