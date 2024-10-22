68cfadbcf1845e2c17ae2d86a73ef3ebeef64068
Isahiah <103591562+IsahiahAL@users.noreply.github.com>
2024-10-22 02:11:00 -0400

    Update script.js
    added tasks

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let task = document.getElementById('task');
 const newTask = document.getElementById('newTask');
 let taskList = ['30 min walk', '30 min bible study', 'full body stretch', 'cardio', 'workout', 'healthy snack', 'healthy meal', '30 min free code camp',
                 '3o min ztm', 'sweep', 'vacuum', 'call mom', 'call dad', 'call andrew', 'movie maker', '30 min piano', 'walk halo', 'clean cat litter',
-                'sweep and mop', 'learn a card trick', 'say five nice things to cheyanne', '30 min easyway'
+                'sweep and mop', 'learn a card trick', 'say five nice things to cheyanne', '30 min easyway read', '30 min easyway audio', 'watch a full episode of FWD',
+                'take a shower', 'drink some tea', '30 min Random Task Generator', '30 min bootstrap tutorial'
 ];
 
 
